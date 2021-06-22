ae3b70f7fa1ea2a6f7e8822ef8dd957fe6dd23a6
Matheus Salomão <mathesbh@gmail.com>
2021-06-22 14:05:14 -0300

    -Inclui link para criar sala -Verifica autenticação com google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useAuth } from '../hooks/useAuth';
+import { useHistory } from 'react-router';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -5,6 +7,17 @@ import '../styles/auth.scss'
 import { Button } from '../components/Button';
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
+
+  async function handleCreateRoom(){
+    if(!user){
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -15,7 +28,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
