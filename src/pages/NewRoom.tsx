869bc5f5ff6f6f62aa23fea2388703e299f07dea
Matheus Salomão <mathesbh@gmail.com>
2021-06-22 12:58:55 -0300

    Adiciona link para home

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss'
@@ -24,7 +25,7 @@ export function NewRoom() {
               Criar sala
             </Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
