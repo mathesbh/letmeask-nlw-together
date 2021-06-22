693cadfc37f40fe0c0153c91fa5936e109141fc3
Matheus Salomão <mathesbh@gmail.com>
2021-06-22 14:04:19 -0300

    Inclui contexto de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -6,8 +7,10 @@ import { NewRoom } from "./pages/NewRoom";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
